ea0b1a179e9d14de23f35b4e8dce2b4056e95dcc
Poonam <poonam@ubuntu.(none)>
2014-12-05 20:52:05 -0600

    Edit judge settings. Modified Controller, model and view

--- app/controllers/judges_controller.rb
+++ app/controllers/judges_controller.rb
@@ -17,9 +17,57 @@ class JudgesController < ApplicationController
   end
 
   def edit
+    id = params[:id]
+    @judge = Judge.find(id)
   end
 
   def update
+    begin
+      judge = Judge.find(params[:id])
+
+      # contestant
+      if params[:judge][:preference].eql? "Contestant"
+
+        p_first_name = params[:judge][:contestant_first_name]
+        p_last_name = params[:judge][:contestant_last_name]
+        contestant = Student.find_by(:first_name=>p_first_name, :last_name=>p_last_name)
+
+        if contestant.nil?
+          raise "Error! Student does not exist"
+        end
+
+        delete_prev_settings(judge)
+        judge_contestant = JudgeContestant.new(judge,contestant)
+        judge_contestant.save!
+      end
+
+      # categories
+      if params[:judge][:preference].eql? "Category"
+        
+        category_name = params[:judge][:category_name]
+        category = Category.find_by(:name=>category_name)        
+
+        delete_prev_settings(judge)
+        judge_category = JudgeCategory.new(judge,category)
+        judge_category.save!
+      end
+
+      # all
+      if params[:judge][:preference].eql? "All"
+        delete_prev_settings(judge)
+      end
+
+      # judge preference
+      judge.update_attributes(judge_params)
+      judge.save!
+      flash[:notice] = "Settings updated successfully."
+      redirect_to(judge_path(judge))
+
+    rescue Exception => e
+      Rails.logger.debug("#{e.inspect}")
+      flash[:notice] = e.message
+      redirect_to(edit_judge_path(judge))
+    end
   end
 
   def show
@@ -45,6 +93,24 @@ class JudgesController < ApplicationController
 
   def judge_params
     params.require(:judge).permit(:first_name, :last_name, :email, :tamu, :preference)
+  end
+
+  def delete_prev_settings(judge)
+    delete_prev_categories(judge)
+    delete_prev_contestants(judge)
+  end
+
+  def delete_prev_categories(judge)
+    JudgeCategory.where(:judge_id => judge).destroy_all
+  end
+
+  def delete_prev_contestants(judge)
+    JudgeContestant.where(:judge_id => judge).destroy_all
+  end
+
+  def throwError(message='')
+    flash[:notice] = message
+    render 'edit'
   end
 
 end
--- app/models/judge.rb
+++ app/models/judge.rb
@@ -4,14 +4,16 @@ class Judge < ActiveRecord::Base
 	validates :email, presence: true, uniqueness: true
 	validates :tamu, inclusion: [true, false] #validates presence of will fail. since rails 4 assume false = absence
 	validates :preference, presence: true
-	validates :preference, inclusion: %w(all categories student)
+	validates :preference, inclusion: %w(All Category Contestant)
 	validates_format_of :email, :with => Authentication::RE_EMAIL_OK, :message => Authentication::MSG_EMAIL_BAD
-
+	attr_accessor :contestant_first_name
+  attr_accessor :contestant_last_name
+  attr_accessor :category_name
 
     after_initialize :set_preference
 
     def set_preference
-    	self.preference ||= "all"
+    	self.preference ||= "All"
     end
 
     def self.getpreference(judgeid)
